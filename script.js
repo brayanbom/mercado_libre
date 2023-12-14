3f513de3b30a932195070fcb95e3e0a8e4546a23
brayan proyecto2 <140023870+MiguelMejia1999@users.noreply.github.com>
2023-12-14 15:15:56 -0500

    tailwind

---
+++ script.js
@@ -0,0 +1,14 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'amarillo-fondo': '#FFE600',
+                'fondole': 'rgba(255, 255, 255, 0.638)'
+             },
+            width: {
+                'paises_ancho': '5px'
+            }
+        }
+    }
+}
